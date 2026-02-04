15e9d2ede1c65204f93b0947650d4a7c595ddfc3
advarjabedian <advarjabedian@gmail.com>
2026-02-04 08:20:52 -0800

    fix

--- fishtech/settings.py
+++ fishtech/settings.py
@@ -15,7 +15,13 @@ import dj_database_url
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Load .env from project root
+load_dotenv(BASE_DIR / '.env')
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
